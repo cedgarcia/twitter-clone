2ea380bbecf7a4d8bb18d763815be7836d5559ec
John Cedrick Garcia <john.garcia@pearlpay.io>
2022-09-21 15:04:26 +0800

    Connected logged in users to database

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth";
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from "next-auth/providers/google";
-
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
 
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+
   providers:[
     GithubProvider({
       
