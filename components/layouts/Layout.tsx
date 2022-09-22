066dcaad60e0bdbc6e33ae1264c4b0da3192f053
John Cedrick Garcia <icedgarcia@gmail.com>
2022-09-22 20:17:40 +0800

    Handling layouts based on pages

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout=({children}:Props)=> {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
